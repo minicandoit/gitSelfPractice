3f1ea9fe95c43b025856c98b67941f4eeea49405
minicandoit <74155239+minicandoit@users.noreply.github.com>
2021-01-26 14:50:29 -0500

    commit3

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,4 +1,7 @@
 public class HelloTest {
     //testing my awesome hello
     //more work for feature c
+    public static void main(String[] args) {
+        System.out.println("how I can merge feature c");
+    }
 }
