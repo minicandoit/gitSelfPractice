9aa5e01d72fe8fba852578785433b7ed8c371a04
minicandoit <74155239+minicandoit@users.noreply.github.com>
2021-01-26 15:17:28 -0500

    commit for movie 1

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -2,5 +2,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello world, B21");
         System.out.println("B21 future SDET learning git");
+        //commit for movie1
     }
 }
